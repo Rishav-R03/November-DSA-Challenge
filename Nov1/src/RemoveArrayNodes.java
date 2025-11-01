97058b9c56320e2d8319d6b4d6dfefe2a0b83bf9
Rishav-R03 <rishav042023@gmail.com>
2025-11-01 20:40:03 +0530

    Nov challenge day 1

--- Nov1/src/RemoveArrayNodes.java
+++ Nov1/src/RemoveArrayNodes.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode{
+    ListNode next;
+    int val;
+    public ListNode(){}
+    public ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedListOps{
+
+    public ListNode insertAtBegin(ListNode head,int val){
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        return newNode;
+    }
+
+    public void displayList(ListNode head){
+        ListNode temp = head;
+        while(temp != null){
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public ListNode insertAtEnd(ListNode head,int val){
+        ListNode newNode = new ListNode(val);
+        if(head == null){
+            insertAtBegin(head,val);
+            return newNode;
+        }
+        ListNode temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+
+        return head;
+    }
+
+    public ListNode removeArrayNodes(int [] arr,ListNode head){
+        Set<Integer> set = new HashSet<>(); // set is used to reduce lookup - O(1)
+        for(int x : arr){
+            set.add(x);
+        }
+        while(head  != null && set.contains(head.val)){
+            head = head.next;
+        }
+        ListNode temp = head;
+        while(temp != null && temp.next != null){
+            while(temp.next!=null && set.contains(temp.next.val)){
+                temp.next = temp.next.next;
+            }
+            temp =temp.next;
+        }
+        return head;
+    }
+
+}
 
 public class RemoveArrayNodes {
+    public static void main(String [] args){
+
+        ListNode head = null;
+        LinkedListOps ops = new LinkedListOps();
+//        head = ops.insertAtBegin(head,10);
+        head = ops.insertAtEnd(head,10);
+        head = ops.insertAtEnd(head,20);
+        head = ops.insertAtEnd(head,30);
+        head = ops.insertAtEnd(head,40);
+        head = ops.insertAtEnd(head,50);
+        ops.displayList(head);
+        int [] arr = {10,20,30};
+        head = ops.removeArrayNodes(arr,head);
+        ops.displayList(head);
+
+    }
 }
--- Nov1/src/ReorderLL.java
+++ Nov1/src/ReorderLL.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
+class ListNode2{
+    ListNode2 next;
+    int val;
+    public ListNode2(){}
+    public ListNode2(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
 public class ReorderLL {
+    public void reorderLL(ListNode2 head){
+        if(head == null) return;
+        //find the middle of linked list using fast-slow pointers
+        ListNode2 slow = head;
+        ListNode2 fast = head;
+
+        while(fast != null && fast.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // step 2: reverse the second half
+        ListNode2 sec = slow.next;
+        slow.next = null;
+        ListNode2 node = null;
+
+        while(sec!=null){
+            ListNode2 temp = sec.next;
+            sec.next = node;
+            node = sec;
+            sec = temp;
+        }
+        // step 3: merge the lists
+        ListNode2 first = head;
+        sec = node;
+        while(sec != null){
+            ListNode2 temp1 = first.next,temp2 = sec.next;
+            first.next = sec;
+            sec.next = temp1;
+            first = temp1;
+            sec = temp2;
+        }
+    }
+    public static void main(String [] args){
+
+    }
 }
